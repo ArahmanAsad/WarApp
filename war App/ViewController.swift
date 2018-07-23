210a95b303e0bcfcf75f58d5e415f41b8904c6ce
A.Rahman <a.rahman.asad22@gmail.com>
2018-07-23 22:42:59 +0200

    commit

--- war App/ViewController.swift
+++ war App/ViewController.swift
@@ -10,16 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCard: UIImageView!
+    
+    @IBOutlet weak var rightCard: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+
+  var leftScore = 0
+  var rightscore = 0
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressButton(_ sender: Any) {
+   
+        let LeftRandomnumber=arc4random_uniform(13)+2
+        let rightRandomNumber=arc4random_uniform(13)+2
+        
+        
+        
+        leftCard.image=UIImage(named: "card\(LeftRandomnumber)")
+        rightCard.image=UIImage(named: "card\(rightRandomNumber)")
+        
+        if LeftRandomnumber > rightRandomNumber{
+            leftScore = leftScore+1
+            leftScoreLabel.text=String(leftScore)
+            
+        }
+        else if rightRandomNumber > LeftRandomnumber{
+            rightscore = rightscore+1
+            rightScoreLabel.text=String(rightscore)
+            
+        }
+        
+    }
+    
 }
 
